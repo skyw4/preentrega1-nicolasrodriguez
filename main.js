aac769e217b350f29f40920176ea3ba08c94ffbd
Nicolas Rodriguez <skyw4.rg@gmail.com>
2023-07-27 00:07:03 -0300

    tercer preentrega, tercer y ultimo commit

--- main.js
+++ main.js
@@ -25,11 +25,8 @@ listaProductos.push(new Producto('1/2 M^2 de Piedra Picada', 'PP2', 3800, 11))
 listaProductos.push(new Producto('1 y 1/2 M^2 de Piedra Picada', 'PP3', 9550, 12))
 console.log(listaProductos)
 
-// Lista ordenada segun los precios de menor a mayor
-    let listaOrdenadaPorPrecio = listaProductos.sort((a, b) => a.precio - b.precio)
-    console.log(listaOrdenadaPorPrecio)
 
-function listaDeProductos(){
+function listaDeProductos() {
     for (const producto of listaProductos) {
         let precioDescuento = Math.round(producto.precio * 0.85);
         let contenedor = document.createElement('div');
@@ -46,8 +43,12 @@ function listaDeProductos(){
     }
 }
 
-function listaOrdenPrecios(){
-    for (const producto of listaOrdenadaPorPrecio) {
+function listaOrdenPrecios() {
+    // Lista ordenada segun los precios de menor a mayor
+    let listaOrdenPorPrecio = listaProductos.sort((a, b) => a.precio - b.precio)
+    console.log(listaOrdenPorPrecio)
+
+    for (const producto of listaOrdenPorPrecio) {
         let precioDescuento = Math.round(producto.precio * 0.85);
         let contenedor = document.createElement('div');
         contenedor.innerHTML = `<h3> Producto: ${producto.nombre}</h3>
@@ -78,47 +79,73 @@ if ((ordenPrecios === 'si') || (ordenPrecios === 'Si') || (ordenPrecios === 'SI'
 
 // Carrito de Compras:
 
-let carrito = []
+const carrito = []
 
 let botones = document.getElementsByClassName('button')
 for (const boton of botones) {
     boton.onclick = () => {
         let productoElegido = listaProductos.find((producto) => producto.orden === parseInt(boton.id))
-    console.log(`Se agrego ${productoElegido.nombre} al carrito`)
-    carrito.push(productoElegido)    
+        console.log(`Se agrego ${productoElegido.nombre} al carrito`)
+        carrito.push(productoElegido)
     }
 }
 let finalizarCompra = document.createElement('button')
-finalizarCompra.innerText = 'Finalizar'
+finalizarCompra.innerText = 'Finalizar Compra'
 finalizarCompra.onclick = () => {
-    console.log(`Gracias por confiar en nosotros! En su carrito hay un total de: ${carrito.length} productos`)
+    alert(`Gracias por confiar en nosotros! En su carrito hay un total de: ${carrito.length} productos`)
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
 document.body.appendChild(finalizarCompra)
 
+let verCarrito = document.createElement('button')
+verCarrito.innerText = 'Mostrar carrito'
+verCarrito.onclick = () => {
+    const carritoStorage = JSON.parse(localStorage.getItem('carrito'))
+    console.log(carritoStorage)
 
-
-let totalProducto = 0;
-
-// creo la funcion para calcular las cuotas
-function calculadorDeCuotas() {
-    let numeroDeCuotas = parseInt(prompt('¿En cuantas cuotas le gustaria realizar su pago?'))
-    while (numeroDeCuotas > 12) {
-        alert('El numero maximo de cuotas es de 12, por favor ingrese un numero entre 1 y 12')
-        numeroDeCuotas = parseInt(prompt('¿En cuantas cuotas le gustaria realizar su pago?'))
-    }
-    let precioConCuotas = totalProducto / numeroDeCuotas
-    alert('El precio total de cada una de las cuotas seria de $' + precioConCuotas.toFixed(2) + '. Muchas gracias por comprar en nuestra tienda!')
+    const carritoNombres = carritoStorage.map((producto) => producto.nombre)
+    alert(`Los productos agregados al carrito son: ${carritoNombres}`)
 }
+document.body.appendChild(verCarrito)
+
+let verPrecios = document.createElement('button')
+verPrecios.innerText = 'Ver Precio Total'
+verPrecios.onclick = () => {
+    const carritoStorage = JSON.parse(localStorage.getItem('carrito'))
+
+    const PreciosCarrito = carritoStorage.map((producto) => producto.precio)
+    const carritoPrecios = PreciosCarrito.reduce((acumulador, elemento) => acumulador + elemento, 0);
+    alert(`El precio total de los productos agregados al carrito es de: $${carritoPrecios * 0.85} (con el descuento del 20%)`)
+}
+document.body.appendChild(verPrecios)
+
+//     const total = numeros.reduce((acumulador, elemento) => acumulador + elemento, 0);
+
+
+// Por ahora quedan anuladas estas funciones, pero no quiero descartarlas del todo hasta estar seguro.
+
+/* // let totalProducto = 0;
+
+// // creo la funcion para calcular las cuotas
+// function calculadorDeCuotas() {
+//     let numeroDeCuotas = parseInt(prompt('¿En cuantas cuotas le gustaria realizar su pago?'))
+//     while (numeroDeCuotas > 12) {
+//         alert('El numero maximo de cuotas es de 12, por favor ingrese un numero entre 1 y 12')
+//         numeroDeCuotas = parseInt(prompt('¿En cuantas cuotas le gustaria realizar su pago?'))
+//     }
+//     let precioConCuotas = totalProducto / numeroDeCuotas
+//     alert('El precio total de cada una de las cuotas seria de $' + precioConCuotas.toFixed(2) + '. Muchas gracias por comprar en nuestra tienda!')
+// }
 
 // antes de ejecutarla le pregunto al usuario si le gustaria calcular el pago en cuotas, de ser asi ejecuto la funcion, y en caso contrario, realizo una salida por alert respondiendo al usuario
-/* let cuotas = prompt('Si le gustaria realizar el pago en cuotas sin interes (limite de 12 cuotas) y quiere que lo calculemos por usted, escriba "Si", en caso contrario, escriba "No"')
-if ((cuotas === 'si') || (cuotas === 'Si') || (cuotas === 'SI')) {
-    calculadorDeCuotas()
-} else if ((cuotas === 'no') || (cuotas === 'NO') || (cuotas === 'No')) {
-    alert('Entendido. Muchas gracias por visitar nuestra tienda!')
-} else {
-    alert('Error: Esta no es una opcion correcta, por favor recargue la pagina si lo necesita.')
-} */
+// let cuotas = prompt('Si le gustaria realizar el pago en cuotas sin interes (limite de 12 cuotas) y quiere que lo calculemos por usted, escriba "Si", en caso contrario, escriba "No"')
+// if ((cuotas === 'si') || (cuotas === 'Si') || (cuotas === 'SI')) {
+//     calculadorDeCuotas()
+// } else if ((cuotas === 'no') || (cuotas === 'NO') || (cuotas === 'No')) {
+//     alert('Entendido. Muchas gracias por visitar nuestra tienda!')
+// } else {
+//     alert('Error: Esta no es una opcion correcta, por favor recargue la pagina si lo necesita.')
+// } */
 
 
 
