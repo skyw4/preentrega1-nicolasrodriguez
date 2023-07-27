a537fe299b79027b5f91ff6ff3bfaa3081f551ba
Nicolas Rodriguez <skyw4.rg@gmail.com>
2023-07-26 21:52:43 -0300

    tercer preentrega, segundo commit

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ class Producto {
         this.orden = orden;
     }
 }
-const listaProductos = [];
+let listaProductos = [];
 
 // ADICIÓN DE PRODUCTOS
 listaProductos.push(new Producto('Cemento', 'CL', 1250, 1))
@@ -25,7 +25,11 @@ listaProductos.push(new Producto('1/2 M^2 de Piedra Picada', 'PP2', 3800, 11))
 listaProductos.push(new Producto('1 y 1/2 M^2 de Piedra Picada', 'PP3', 9550, 12))
 console.log(listaProductos)
 
+// Lista ordenada segun los precios de menor a mayor
+    let listaOrdenadaPorPrecio = listaProductos.sort((a, b) => a.precio - b.precio)
+    console.log(listaOrdenadaPorPrecio)
 
+function listaDeProductos(){
     for (const producto of listaProductos) {
         let precioDescuento = Math.round(producto.precio * 0.85);
         let contenedor = document.createElement('div');
@@ -40,6 +44,35 @@ console.log(listaProductos)
         contenedor.style.margin = '15px'
         document.body.appendChild(contenedor)
     }
+}
+
+function listaOrdenPrecios(){
+    for (const producto of listaOrdenadaPorPrecio) {
+        let precioDescuento = Math.round(producto.precio * 0.85);
+        let contenedor = document.createElement('div');
+        contenedor.innerHTML = `<h3> Producto: ${producto.nombre}</h3>
+                                    <p>  Precio: $${producto.precio}</p>
+                                    <b>  Precio con descuento: $${precioDescuento}</b>
+                                    <p>ID: ${producto.orden}</
+                                    <br>
+                                    <button id="${producto.orden}"  class="button">Agregar al carrito</button>`
+        contenedor.style.border = '2px solid grey'
+        contenedor.style.padding = '15px'
+        contenedor.style.margin = '15px'
+        document.body.appendChild(contenedor)
+    }
+}
+
+let ordenPrecios = prompt('¿Desea ordenar los productos por precio de menor a mayor? Responder con "Si" o "No"')
+if ((ordenPrecios === 'si') || (ordenPrecios === 'Si') || (ordenPrecios === 'SI')) {
+    listaOrdenPrecios()
+} else if (((ordenPrecios === 'no') || (ordenPrecios === 'NO') || (ordenPrecios === '   NO'))) {
+    alert('De acuerdo, no hay problema! Aqui esta la lista de precios por orden predeterminado.');
+    listaDeProductos()
+} else {
+    alert('Error: Esta no es una opcion correcta.')
+}
+
 
 
 
@@ -55,12 +88,12 @@ for (const boton of botones) {
     carrito.push(productoElegido)    
     }
 }
-/* let finalizarCompra = document.createElement('button')
+let finalizarCompra = document.createElement('button')
 finalizarCompra.innerText = 'Finalizar'
 finalizarCompra.onclick = () => {
     console.log(`Gracias por confiar en nosotros! En su carrito hay un total de: ${carrito.length} productos`)
 }
-document.body.appendChild(finalizarCompra) */
+document.body.appendChild(finalizarCompra)
 
 
 
@@ -85,20 +118,6 @@ if ((cuotas === 'si') || (cuotas === 'Si') || (cuotas === 'SI')) {
     alert('Entendido. Muchas gracias por visitar nuestra tienda!')
 } else {
     alert('Error: Esta no es una opcion correcta, por favor recargue la pagina si lo necesita.')
-} */
-
-function OrdenarPorPrecio() {
-    let ordenarPrecio = listaProductos.sort((a, b) => a.precio - b.precio)
-    console.log(ordenarPrecio)
-}
-
-/* let ordenPrecios = prompt('¿Desea ordenar los productos por precio de menor a mayor? Responder con "Si" o "No"')
-if ((ordenPrecios === 'si') || (ordenPrecios === 'Si') || (ordenPrecios === 'SI')) {
-    OrdenarPorPrecio()
-} else if (((ordenPrecios === 'no') || (ordenPrecios === 'NO') || (ordenPrecios === '   NO'))) {
-    alert('De acuerdo, no hay problema! Que tenga un buen dia!')
-} else {
-    alert('Error: Esta no es una opcion correcta.')
 } */
 
 
